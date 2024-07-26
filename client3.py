063ff6038d2592ec9aa1d2926bd1e07407194887
Nathanage3 <sisay954@gmail.com>
2024-07-26 22:19:58 +0300

    new updates on client3.py and server3.py

--- client3.py
+++ client3.py
@@ -35,25 +35,48 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    """ Get ratio of price_a and price_b.
+
+    Args:
+        price_a (float): The first price
+        price_b (float): The second price
+
+    Returns:
+        (float): the ratio of price_a to price_b,
+                 otherwise None if price_b is 0.
+    
+    """
+
+    if not isinstance(price_a, (float, int)):
+        raise ValueError("The value of must be a number.")
+    if not isinstance(price_b, (float, int)):
+        raise ValueError("The value of must be a number.")
+    try:
+        if price_b == 0:
+            return None
+        else:
+            result = price_a / price_b
+            return result
+    except ZeroDivisionError:
+        return None
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
